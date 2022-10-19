f332361b615ab9576356544d1db95709bc510867
Jason Raubenheimer <raubenheimer.jason@gmail.com>
2022-10-19 15:53:05 +0100

    Trying with a larger matrix (10x3) x (3x7)

--- matrix_mul/matrix_mul.cpp
+++ matrix_mul/matrix_mul.cpp
@@ -14,8 +14,9 @@ int main()
 	//std::cout << "Hello World!\n";
 
 	//std::vector<std::vector<int>> A{ {1,2},{3,4},{5,6} };
-	std::vector<std::vector<int>> A{ {1,2},{3,4} };
-	std::vector<std::vector<int>> B{ {5,6},{7,8} };
+	//std::vector<std::vector<int>> A{ {1,2},{3,4} };
+	std::vector<std::vector<int>> A{ {1,2,9},{3,4,9},{1,2,9},{3,4,9},{1,2,9},{3,4,9},{1,2,9},{3,4,9},{1,2,9},{3,4,9} };
+	std::vector<std::vector<int>> B{ {5,6,9,9,9,9,9},{7,8,9,9,9,9,9},{7,8,9,9,9,9,9} };
 
 	std::cout << "A." << std::endl;
 	mat_print(A);
