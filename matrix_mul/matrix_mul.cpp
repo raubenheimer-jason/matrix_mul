0e4a4aa064791873d3343308d7ed15bbe1c5d3b1
Jason Raubenheimer <raubenheimer.jason@gmail.com>
2022-10-19 16:39:47 +0100

    random matrix generator - not working 100% yet

--- matrix_mul/matrix_mul.cpp
+++ matrix_mul/matrix_mul.cpp
@@ -7,7 +7,8 @@
 
 int mat_mul(const std::vector<std::vector<int>>& A, const std::vector<std::vector<int>>& B, std::vector<std::vector<int>>& C);
 bool mat_mul_check_dims(const std::vector<std::vector<int>>& A, const  std::vector<std::vector<int>>& B);
-void mat_print(const std::vector<std::vector<int>>& A);
+void mat_print(const std::vector<std::vector<int>>& X);
+void mat_rand(const int rows, const int cols, std::vector<std::vector<int>>& X, int start = 0, int end = 9); // make random matrix
 
 int main()
 {
@@ -15,8 +16,14 @@ int main()
 
 	//std::vector<std::vector<int>> A{ {1,2},{3,4},{5,6} };
 	//std::vector<std::vector<int>> A{ {1,2},{3,4} };
-	std::vector<std::vector<int>> A{ {1,2,9},{3,4,9},{1,2,9},{3,4,9},{1,2,9},{3,4,9},{1,2,9},{3,4,9},{1,2,9},{3,4,9} };
-	std::vector<std::vector<int>> B{ {5,6,9,9,9,9,9},{7,8,9,9,9,9,9},{7,8,9,9,9,9,9} };
+	//std::vector<std::vector<int>> A{ {1,2,9},{3,4,9},{1,2,9},{3,4,9},{1,2,9},{3,4,9},{1,2,9},{3,4,9},{1,2,9},{3,4,9} };
+	//std::vector<std::vector<int>> B{ {5,6,9,9,9,9,9},{7,8,9,9,9,9,9},{7,8,9,9,9,9,9} };
+
+	std::vector<std::vector<int>> A;
+	std::vector<std::vector<int>> B;
+
+	mat_rand(10, 3, A);
+	mat_rand(3, 7, B);
 
 	std::cout << "A." << std::endl;
 	mat_print(A);
@@ -24,7 +31,7 @@ int main()
 	mat_print(B);
 
 
-	std::vector<std::vector<int>> C{ {} };
+	std::vector<std::vector<int>> C;
 
 	auto start = std::chrono::high_resolution_clock::now();
 	int result = mat_mul(A, B, C);
@@ -32,11 +39,11 @@ int main()
 
 	auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
 
-	//if (result == -1)
-		//std::cout << "error" << std::endl;
+	if (result == -1)
+		std::cout << "dim error\n" << std::endl;
 
 	//std::cout << "C." << std::endl;
-	std::cout << "C.";
+	std::cout << "C." << std::endl;
 	mat_print(C);
 
 	// To get the value of duration use the count()
@@ -44,11 +51,28 @@ int main()
 	std::cout << "time: " << duration.count() << " us" << std::endl;
 }
 
-void mat_print(const std::vector<std::vector<int>>& A)
+// make random matrix
+void mat_rand(const int rows, const int cols, std::vector<std::vector<int>>& X, int start, int end)
+{
+	//X = { {} };
+
+	for (int r{}; r < rows; ++r)
+	{
+		std::vector<int> row;
+		for (int c{}; c < cols; ++c)
+		{
+			int random = start + (rand() % end);
+			row.push_back(1);
+		}
+		X.push_back(row);
+	}
+}
+
+void mat_print(const std::vector<std::vector<int>>& X)
 {
 	//std::cout << "printing..." << std::endl;
 
-	for (auto& r : A)
+	for (auto& r : X)
 	{
 		for (auto& e : r)
 		{
@@ -64,6 +88,8 @@ bool mat_mul_check_dims(const std::vector<std::vector<int>>& A, const  std::vect
 {
 	// num columns of A must equal num rows of B
 
+	//std::cout << A.at(0).size() << "  " << B.size() << std::endl;
+
 	if (A.at(0).size() == B.size())
 		return true;
 
@@ -78,7 +104,7 @@ int mat_mul(const std::vector<std::vector<int>>& A, const std::vector<std::vecto
 		return -1; // error
 
 	// C holds the result, make sure it is empty
-	C = { {} };
+	//C = { {} };
 
 	// for the first element of result, loop over each column in the first row of A, loop over each row in the first column of B
 
