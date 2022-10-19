518f6455d2b291010d76ff33b6513eaff64dc28f
Jason Raubenheimer <raubenheimer.jason@gmail.com>
2022-10-19 15:46:31 +0100

    Correct result with (3x2) x (2x2)

--- matrix_mul/matrix_mul.cpp
+++ matrix_mul/matrix_mul.cpp
@@ -12,8 +12,8 @@ int main()
 {
 	//std::cout << "Hello World!\n";
 
-	//std::vector<std::vector<int>> A{ {1,2},{3,4},{5,6} };
-	std::vector<std::vector<int>> A{ {1,2},{3,4} };
+	std::vector<std::vector<int>> A{ {1,2},{3,4},{5,6} };
+	//std::vector<std::vector<int>> A{ {1,2},{3,4} };
 	std::vector<std::vector<int>> B{ {5,6},{7,8} };
 
 	std::cout << "A." << std::endl;
