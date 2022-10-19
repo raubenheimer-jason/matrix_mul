816b623b2d2edea1da6349f522e6f8b4bb89f3ad
Jason Raubenheimer <raubenheimer.jason@gmail.com>
2022-10-19 15:21:45 +0100

    Running but result is wrong

--- matrix_mul/matrix_mul.cpp
+++ matrix_mul/matrix_mul.cpp
@@ -15,9 +15,9 @@ int main()
 	std::vector<std::vector<int>> A{ {1,2},{3,4},{5,6} };
 	std::vector<std::vector<int>> B{ {5,6},{7,8} };
 
-	std::cout << "A" << std::endl;
+	std::cout << "A." << std::endl;
 	mat_print(A);
-	std::cout << "B" << std::endl;
+	std::cout << "B." << std::endl;
 	mat_print(B);
 
 
@@ -25,9 +25,11 @@ int main()
 
 	int result = mat_mul(A, B, C);
 
-	if (result == -1)
-		std::cout << "error" << std::endl;
+	//if (result == -1)
+		//std::cout << "error" << std::endl;
 
+	//std::cout << "C." << std::endl;
+	std::cout << "C.";
 	mat_print(C);
 
 
@@ -46,6 +48,7 @@ void mat_print(const std::vector<std::vector<int>>& A)
 		}
 		std::cout << std::endl;
 	}
+	std::cout << std::endl;
 }
 
 
@@ -74,17 +77,44 @@ int mat_mul(const std::vector<std::vector<int>>& A, const std::vector<std::vecto
 	size_t C_cols{ B.at(0).size() }; // Num of cols in B
 	size_t C_rows{ A.size() }; // Num of rows in A
 
-	std::cout << "C_cols: " << C_cols << std::endl;
-	std::cout << "C_rows: " << C_rows << std::endl;
+	size_t B_rows{ B.size() }; // Num of rows in B
+	size_t A_cols{ A.at(0).size() }; // Num of cols in A
 
 
-	size_t num_elements{};
-	for (size_t i{}; i < (C_cols + C_rows); ++i)
+	//std::cout << "C_cols: " << C_cols << std::endl;
+	//std::cout << "C_rows: " << C_rows << std::endl;
+
+	//std::cout << std::endl;
+
+	// iterate over each row
+	for (size_t r{}; r < C_rows; ++r)
 	{
-		num_elements++;
-	}
+		std::vector<int> row;
+		// iterate over each column
+		for (size_t c{}; c < C_cols; ++c)
+		{
+			//std::cout << "r: " << r << " c: " << c << "  ";
+			//int res{A.at(r)}
 
-	std::cout << "num_elements: " << num_elements << std::endl;
+			int element_result{};
+
+			// then iterate over each column in A for the current row of C
+			// and each row in B for the current column of C
+			for (size_t cA{}, rB{}; cA < A_cols; ++cA, ++rB) // can also work for rB < B_rows
+			{
+				element_result += A.at(r).at(cA) * B.at(c).at(rB);
+
+				//for (size_t rB{}; rB < B_rows; ++rB)
+				//{
+				//	element_result += A.at(r).at(cA) * B.at()
+				//}
+			}
+
+			row.push_back(element_result);
+		}
+		C.push_back(row);
+		//std::cout << std::endl;
+	}
 
 	return 0;
 }
